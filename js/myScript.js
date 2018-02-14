6d1cf1bf0bcca25c9e72c5c12e1543c2728889f8
Susana Piñero <susananzth@gmail.com>
2018-02-13 21:37:15 -0400

    Agregando jQuery, modificando header y about me, colocando icono de la página, quitando article de portfolio

---
+++ js/myScript.js
@@ -0,0 +1,38 @@
+$(function(){
+        $('a.smoothScroll').smoothScroll({
+          offset: -80,		  
+		  scrollTarget: $(this).val()
+	   });
+       
+       // Waypoints
+       $('.post_article').waypoint(  
+        function(direction) {
+        if (direction ==='down') {            
+            var wayID = $(this).attr('id');            
+        } else {
+            var previous = $(this).prev();
+            var wayID = $(previous).attr('id');                    
+        }
+            $('.current').removeClass('current');
+            $('#main_nav a[href=#'+wayID+']').addClass('current');
+        }, { offset: '40%' });
+       
+       
+       
+       /// StickNav  
+       var stickyNavTop = $('.nav').offset().top;  
+  
+        var stickyNav = function(){  
+        var scrollTop = $(window).scrollTop();  
+       
+        if (scrollTop > stickyNavTop) {   
+            $('.nav').addClass('fixed-nav');  
+        } else {  
+            $('.nav').removeClass('fixed-nav');   
+        }  
+    };       
+    stickyNav(); 
+       $(window).scroll(function() {
+            stickyNav();  
+        });  
+    });
