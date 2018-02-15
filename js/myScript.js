a93b6ed789bd07597e2fa2427fef009f18b37ed7
Susana Piñero <susananzth@gmail.com>
2018-02-15 19:18:56 -0400

    Modificando animaciones a el menu, navbar y servicios y añadiendo background en el header

--- js/myScript.js
+++ js/myScript.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
 	$(".menubtn").on('click', function(){
-    		$('#sideNav').animate({width: "100%"}, 500);
+    	$('#sideNav').animate({width: "100%"}, 500);
     });
     $(".closebtn").on('click', function(){
-    		$('#sideNav').animate({width: "0"}, 500);
+    	$('#sideNav').animate({width: "0"}, 500);
     });
-    $('#services').addClass('animated fadeInUp');
-    /*var services = document.getElementById("services");
-    		services.className += " animated fadeInUp";*/
-
+    $(".menu-link").on('click', function(){
+    	$('#sideNav').animate({width: "0"}, 500);
+    });
 	window.onscroll = function() {scrollFunction()};
     function scrollFunction() {
-	    if (document.body.scrollTop > 133 || document.documentElement.scrollTop > 133) {
+    	if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+			$('.services-speed').addClass('animated-0-8s fadeInUp');
+		    $('.services-responsive').addClass('animated-0-9s fadeInUp');
+    	}
+    	if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+		    $('.services-dynamic').addClass('animated-0-9s fadeInUp');
+		    $('.services-intuitive').addClass('animated-1s fadeInUp');
+    	}
+    	if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+		    $('.services-love').addClass('animated-1-1s fadeInUp');
+    	}
+	    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
 	        document.getElementById("name").style.display = "block";
 	        document.getElementById("nav").style.position = "fixed";
 	    } else {
@@ -19,4 +29,8 @@ $(document).ready(function(){
 	        document.getElementById("nav").style.position = "relative";
 	    }
 	}
+	$('form').on('submit', sendForm);
+        function sendForm(ev) {
+            ev.preventDefault();
+        }
 });
