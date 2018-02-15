431ba5688081aacaf37b13bcf0edf34ee871569b
Susana Piñero <susananzth@gmail.com>
2018-02-15 17:31:59 -0400

    Añadiendo animaciones a el menu, navbar y servicios

--- js/myScript.js
+++ js/myScript.js
@@ -1,20 +1,22 @@
+$(document).ready(function(){
+	$(".menubtn").on('click', function(){
+    		$('#sideNav').animate({width: "100%"}, 500);
+    });
+    $(".closebtn").on('click', function(){
+    		$('#sideNav').animate({width: "0"}, 500);
+    });
+    $('#services').addClass('animated fadeInUp');
+    /*var services = document.getElementById("services");
+    		services.className += " animated fadeInUp";*/
 
-    function openNav() {
-      document.getElementById("sideNav").style.width = "100%";
-      }
-
-      function closeNav() {
-          document.getElementById("sideNav").style.width = "0";
-      }
-
-    window.onscroll = function() {scrollFunction()};
-
+	window.onscroll = function() {scrollFunction()};
     function scrollFunction() {
-    if (document.body.scrollTop > 133 || document.documentElement.scrollTop > 133) {
-        document.getElementById("name").style.display = "block";
-        document.getElementById("nav").style.position = "fixed";
-    } else {
-        document.getElementById("name").style.display = "none";
-        document.getElementById("nav").style.position = "relative";
-    }
-}
+	    if (document.body.scrollTop > 133 || document.documentElement.scrollTop > 133) {
+	        document.getElementById("name").style.display = "block";
+	        document.getElementById("nav").style.position = "fixed";
+	    } else {
+	        document.getElementById("name").style.display = "none";
+	        document.getElementById("nav").style.position = "relative";
+	    }
+	}
+});
