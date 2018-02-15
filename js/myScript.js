c2e6792629d2b2eeebbba349f3b9a0007b8d64ca
Susana Piñero <susananzth@gmail.com>
2018-02-15 14:06:08 -0400

    Modificando los efectos de la navbar cuando hago scroll, modificando las mediaquery en el div de menu

--- js/myScript.js
+++ js/myScript.js
@@ -1,48 +1,20 @@
-$(function(){
-        $('a.smoothScroll').smoothScroll({
-          offset: -80,		  
-		  scrollTarget: $(this).val()
-	   });
-       
-       // Waypoints
-       $('.about-me').waypoint(  
-        function(direction) {
-        if (direction ==='down') {            
-            var wayID = $(this).attr('id');            
-        } else {
-            var previous = $(this).prev();
-            var wayID = $(previous).attr('id');                    
-        }
-            $('.current').removeClass('current');
-            $('#nav a[href=#'+wayID+']').addClass('current');
-        }, { offset: '40%' });
-       
-       
-       
-       /// StickNav  
-       var stickyNavTop = $('.nav').offset().top;  
-  
-        var stickyNav = function(){  
-        var scrollTop = $(window).scrollTop();  
-       
-        if (scrollTop > stickyNavTop) {   
-            $('.nav').addClass('fixed-nav');  
-        } else {  
-            $('.nav').removeClass('fixed-nav');   
-        }  
-    };       
-    stickyNav(); 
-       $(window).scroll(function() {
-            stickyNav();  
-        });
 
     function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    }
+      document.getElementById("sideNav").style.width = "100%";
+      }
 
-    function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("main").style.marginLeft= "0";
-    } 
-    });
+      function closeNav() {
+          document.getElementById("sideNav").style.width = "0";
+      }
+
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 133 || document.documentElement.scrollTop > 133) {
+        document.getElementById("name").style.display = "block";
+        document.getElementById("nav").style.position = "fixed";
+    } else {
+        document.getElementById("name").style.display = "none";
+        document.getElementById("nav").style.position = "relative";
+    }
+}
